8ead78f81ec876de0cead68cdb4750ebb4e3c457
Tyler <Tyl3r.s@outlook.com>
2022-06-23 21:49:52 -0400

    collects current city weather and puts it on screen

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,3 @@
-// var apiKey = `96f33839c85744a54cc32451f4cf28cb`;
-// var userFormEl = document.querySelector(`.city-search`);
-// var cityInputEl = document.querySelector(`.search-bar`);
-
-// var formSubmitHandler = function (event) {
-
-//   event.preventDefault();
-
-//   // get value from input element
-//   var city = cityInputEl.value.trim();
-
-//   if (city) {
-//       getCity(city);
-//       cityInputEl.value = "";
-//   } else {
-//       alert("Please enter a city name!");
-//   }
-// }
-
-// var getCity = function(city) {
-//     // format the github api url
-//     var apiUrl ="https://api.openweathermap.org/data/2.5/onecall?lat=" + "43.65" + "&lon=" + "90.04" + "&units=metric&exclude=minutely&appid=" + apiKey;
-  
-//     // make a get request to url
-//     fetch(apiUrl).then(function(response) {
-//       response.json().then(function(data) {
-//         console.log(data);
-//       });
-//     });
-//   };
-  
-// var displayWeather = function() {
-
-// }
-
-// userFormEl.addEventListener("submit", formSubmitHandler);
-
 // toronto weather using city search (does not give UV index)
 // https://api.openweathermap.org/data/2.5/weather?q=toronto&appid=96f33839c85744a54cc32451f4cf28cb 
 
@@ -44,18 +7,42 @@
 // get toronto (or any city) lat and lon by searching name
 // http://api.openweathermap.org/geo/1.0/direct?q=Toronto&limit=5&appid=96f33839c85744a54cc32451f4cf28cb
 
+var lat = [];
+
+var lon = [];
+
+
+// handles weather data for current day
 let weather = {
     apiKey: "96f33839c85744a54cc32451f4cf28cb",
     fetchWeather: function(lat, lon) {
         fetch(
-            "https://api.openweathermap.org/data/2.5/onecall?lat="
+            // finds weather data based on lon and lat input
+            "https://api.openweathermap.org/data/2.5/onecall?units=metric&lat="
             + lat
             + "&lon="
             + lon
             + "&exclude=minutely,hourly,daily&appid=" 
             + this.apiKey
             )
+            // takes all that data, parses, and gives to displayWeather
             .then((response) => response.json())
-            .then((data) => console.log(data));
+            .then((data) => this.displayWeather(data));
+        },
+        // grabs specified parts of the data just collected and assigns to variable
+        displayWeather: function(data) {
+            const name = data.timezone;
+            const temp = data.current.temp;
+            const wind = data.current.wind_speed;
+            const humidity = data.current.humidity;
+            const UVindex = data.current.uvi;
+            const icon = data.current.weather[0].icon;
+            // puts the correct data on the screen
+            document.querySelector(".city-name").innerText = "Weather in " + name;
+            document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+icon+".png";
+            document.querySelector(".temp").innerText = "Temperature: "+temp+"°C";            
+            document.querySelector(".wind").innerText = "Wind Speed: "+wind+"m/s";
+            document.querySelector(".humidity").innerText = "Humidity: "+humidity+"%";
+            document.querySelector(".UVindex").innerText = "UV index: "+UVindex;
         }
 }
